48d58bb5ef6595a98c4aeb9e6684af197edfb701
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-02-14 20:41:23 -0600

    started a linked list class, implemented Node and linked list class and an append method

--- Datastructures/try.cpp
+++ Datastructures/try.cpp
@@ -1,19 +1,39 @@
-// Datastructures.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 
-int main()
+class Node
 {
-    std::cout << "Hello World!\n";
-}
+public:
+    int data;
+    Node* next = nullptr;
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+    Node(int value) : data(value), next(nullptr) {}
+};
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+class LinkedList
+{
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void append(int data)
+    {
+        if (head == nullptr)
+        {
+            head = new Node(data);
+            return;
+        }
+        Node* last_node = head;
+        while (last_node->next != nullptr)
+        {
+            last_node = last_node->next;
+        }
+        last_node->next = new Node(data);
+    }
+
+    void print()
+    {
+
+    }
+};
