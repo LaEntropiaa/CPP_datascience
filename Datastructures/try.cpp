c7db1a8eca74f4bbd6c78b9348ca6dfac1ebbbac
Alonso887 <138240277+Alonso887@users.noreply.github.com>
2024-02-15 19:45:37 -0600

    Added insert function, it works but error handling needs to improve, it just throws a message instead of crashing the program

--- Datastructures/try.cpp
+++ Datastructures/try.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 template <typename T>
 class Node
@@ -41,6 +42,38 @@ public:
         head = new_node;
     }
 
+    void insert(T data, int index)
+    {
+        Node<T>* new_node = new Node<T>(data);
+        Node<T>* current_node = head;
+        int current_index = 0;
+        try{
+        if (current_index < 0)
+        {
+            throw "Insert method doesn't support negative index.";
+        }
+        while (current_index < index)
+        {
+            if (current_node == nullptr or current_node->next == nullptr)
+            {
+                throw "Index out of range";
+            }
+            current_node = current_node->next;
+            current_index++;
+        }
+        if (current_node == nullptr or current_node->next == nullptr)
+        {
+            throw "Index out of range";
+        }
+        Node<T>* next_node = current_node->next;
+        current_node->next = new_node;
+        new_node->next = next_node;
+        }catch (const char* e){
+            std::cout << "Error: " << e << "\n";
+            std::cout << "Insertion failed" << "\n";
+        }
+    }
+ 
     void print()
     {
         if (head == nullptr)
@@ -80,6 +113,7 @@ int main()
     list.append(40);
     list.append(23);
     list.pre_append(55);
+    list.insert(56, 5);
 
     list.print();
 
